f39c4a1b79fb1cb5df677e49a31f57b79aedd1dd
Goldcoronet <144882839+Goldcoronet@users.noreply.github.com>
2025-08-07 11:02:56 +0530

    Pokemon_dictionary_V1
    first version : 
    *Can fetch spirties based on pokemon name/index
    *Will provide a description for pokemon

---
+++ index.js
@@ -0,0 +1,66 @@
+let currentid = 1;
+
+const nameInput = document.getElementById("name");
+const infoText= document.getElementById("desc-box");
+
+async function pokesearch(inputval = null) {
+    try {
+        const name = inputval !== null ? inputval.toString().toLowerCase() : nameInput.value.toLowerCase();
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+
+        if (!response.ok) throw new Error("Not found");
+
+        const data = await response.json();
+        const sprite = data.sprites.front_default;
+
+        //To fetch Description
+        const speciesRes = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${name}`);
+        const speciesData = await speciesRes.json();
+        const info = speciesData.flavor_text_entries.find(entry => entry.language.name === 'en');
+
+        const des = info ? info.flavor_text.replace(/\f/g, ' ') : 'No description available.';
+        infoText.textContent=des;
+
+        currentid = data.id;
+
+        const pokeimg = document.getElementById("poke-sprite");
+        pokeimg.src = sprite;
+        pokeimg.style.display = "block";
+
+        nameInput.disabled = false;
+        nameInput.value = "";
+        
+    } catch (error) {
+        const pokeimg = document.getElementById("poke-sprite");
+        pokeimg.src = "Pokemon-Pokeball-PNG-HD-Image.png"
+
+        console.error(error);
+        nameInput.disabled = false;
+    }
+}
+
+function nextpoke() {
+    pokesearch(currentid + 1);
+}
+
+function prevpoke() {
+    if (currentid !== 1) pokesearch(currentid - 1);
+}
+
+document.getElementById("poke-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    pokesearch();
+});
+
+document.addEventListener("keydown", (event) => {
+
+    const active = document.activeElement;
+    if (active && (active.tagName === "INPUT" || active.tagName === "TEXTAREA")) return;
+
+    if (event.key === "ArrowRight") {
+        nextpoke();
+    } else if (event.key === "ArrowLeft") {
+        prevpoke();
+    }
+});
+
